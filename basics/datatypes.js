a001095662ca3cf251cd6f33393630be5f783292
dhanu0607 <dhanupoojary0607@gmail.com>
2025-12-02 07:56:32 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3);//we are using mode js ,not webbroweser
+
+console.log(3+3);
+console.log("Dhanush");
+
+let name = "Dhanush";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+//number  => 2 to the power 53
+//bigint 
+//string => ""
+//boolean  => true/false
+//null => standalone value
+//undefined => variable without value
+//symbol => unique identifiers
+
+//object 
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
