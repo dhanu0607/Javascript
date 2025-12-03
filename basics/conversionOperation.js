67b4ff49fa8a7a05b62f573c745fe8d5d7806bab
Dhanush <dhanupoojary0607@gmail.com>
2025-12-03 07:29:46 +0000

    conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "dhanush"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=>Nan(not a number)
+//true=>1 ; false=>0
+
+let isLoggedIn = "dhanush"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"dhanush"=>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber);
+;
+
